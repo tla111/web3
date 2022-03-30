d5d46e277728ceb5442bb78f98f135299e63c95f
Tim La <tlamarketing111@gmail.com>
2022-03-30 14:52:23 -0400

    created array of navbar items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import logo from '../../images/logo.png';
+import logo from '../images/logo.png';
+
+const NavbarItem = ({ title, classProps }) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
 
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
-
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                {["Market", "Exchanges", "Tutorials", "Wallets"].map((item, index) => (
+                    <NavbarItem key={item + index} title={item} />
+                ))}
+            </ul>
         </nav>
     )
 }
