6d58e237b2d77bc59484c53b3364ec67d3ddaf1d
Tim La <tlamarketing111@gmail.com>
2022-03-30 14:53:51 -0400

    login button created & styled

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,6 +22,9 @@ const Navbar = () => {
                 {["Market", "Exchanges", "Tutorials", "Wallets"].map((item, index) => (
                     <NavbarItem key={item + index} title={item} />
                 ))}
+                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                    Login
+                </li>
             </ul>
         </nav>
     )
