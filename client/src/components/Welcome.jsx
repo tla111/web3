522b5dd3380d7dd5a3ff5858f8c4db0dafe3a35d
Tim La <tlamarketing111@gmail.com>
2022-03-31 11:00:06 -0400

    input component styled & reusable

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,9 +7,14 @@ import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({ placeholder }) => (
+const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
         placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorpism"
     />
 );
 
