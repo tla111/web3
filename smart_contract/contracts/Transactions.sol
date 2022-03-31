203bb0eb96c12baa04bfd81d247348594eebbe6d
Tim La <tlamarketing111@gmail.com>
2022-03-31 11:41:24 -0400

    created logic for smart contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,9 +3,9 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
-    event Trasnfer(
+    event Transfer(
         address from,
         address receiver,
         uint256 amount,
@@ -25,17 +25,22 @@ contract Transactions {
 
     TransferStruct[] transactions;
 
-    function addToBlockchain() public {}
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
 
     function getAllTransactions()
         public
         view
         returns (TransferStruct[] memory)
     {
-        // return transactions;
+        return transactions;
     }
 
     function getTransactionCount() public view returns (uint256) {
-        // return transactionCount;
+        return transactionCount;
     }
 }
