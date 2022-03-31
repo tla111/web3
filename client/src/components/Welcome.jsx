7a37a7ef54005653753f6f5dee3904ad9d4d6ec6
Tim La <tlamarketing111@gmail.com>
2022-03-31 11:09:02 -0400

    send now button designed

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -14,12 +14,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
         step="0.0001"
         value={value}
         onChange={(e) => handleChange(e, name)}
-        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorpism"
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+
     />
 );
 
 const Welcome = () => {
     const connectWallet = () => {
+
+    };
+
+    const handleSubmit = () => {
 
     };
 
@@ -86,6 +91,26 @@ const Welcome = () => {
 
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
                         <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => { }} />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => { }} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => { }} />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => { }} />
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+
+                        {false ? (
+                            <Loader />
+                        ) : (
+
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                            >
+                                Send Now
+                            </button>
+
+                        )}
                     </div>
 
                 </div>
