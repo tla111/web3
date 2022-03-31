e4c2ed461715328467db9b93bfc13e220fec4ecc
Tim La <tlamarketing111@gmail.com>
2022-03-31 11:35:27 -0400

    created logic for transactions

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCounter;
+
+    event Trasnfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain() public {}
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        // return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        // return transactionCount;
+    }
+}
