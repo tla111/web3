bd0847cffdee9931fec22ead9298d794abdae6c6
Tim La <tlamarketing111@gmail.com>
2022-03-31 10:45:57 -0400

    first cell and connect wallet button created

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,7 +5,13 @@ import { BsInfoCircle } from 'react-icons/bs';
 
 import { Loader } from './';
 
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
 const Welcome = () => {
+    const connectWallet = () => {
+
+    };
+
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:20 py-12 px-4">
@@ -16,6 +22,20 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
+                    <button
+                        type="button"
+                        onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd"
+
+                    >
+                        <p className="text-white text-base font-semibold">Connect Wallet</p>
+                    </button>
+
+                    <div className="grid sm:grid-cols-3 grid-cols2 w-full mt-10">
+                        <div className={`rounded-tl-2xl ${commonStyles}`}>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
