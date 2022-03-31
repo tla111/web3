79d7e1ca3f025051e71202ef6321105d4dabec50
Tim La <tlamarketing111@gmail.com>
2022-03-31 10:48:47 -0400

    box of cards created

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -33,7 +33,22 @@ const Welcome = () => {
 
                     <div className="grid sm:grid-cols-3 grid-cols2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
-
+                            Reliability
+                        </div>
+                        <div className={commonStyles}>
+                            Security
+                        </div>
+                        <div className={`rounded-tr-2xl ${commonStyles}`}>
+                            Ethereum
+                        </div>
+                        <div className={`rounded-bl-2xl ${commonStyles}`}>
+                            Web 3.0
+                        </div>
+                        <div className={commonStyles}>
+                            Low fees
+                        </div>
+                        <div className={`rounded-br-2xl ${commonStyles}`}>
+                            Web 3.0
                         </div>
                     </div>
                 </div>
