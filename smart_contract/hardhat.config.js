262d2245077d16491e326a00a8bdaffc9a8efc3b
Tim La <tlamarketing111@gmail.com>
2022-03-31 12:10:17 -0400

    hardhat config file created

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,1 +1,11 @@
-// https://eth-ropsten.alchemyapi.io/v2/xy3SnTeTRq9BiOqN4Mk9XsjptYiKPwaE
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/xy3SnTeTRq9BiOqN4Mk9XsjptYiKPwaE",
+      accounts: ["310da03630434d41464ac1f44b182ea1b9a15b0b3ee6b4bac51e7d762f658144"],
+    }
+  }
+}
