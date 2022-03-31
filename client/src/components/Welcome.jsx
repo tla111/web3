041e9530037a7fa4cd5230863cf5f4384330b8ea
Tim La <tlamarketing111@gmail.com>
2022-03-31 10:40:31 -0400

    header in welcome section implemented

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
 
 const Welcome = () => {
     return (
-        <div>Welcome</div>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                        Send Crypto <br /> across the world
+                    </h1>
+                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
