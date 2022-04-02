aca7974998686b2a75df3a421b8c2a4a1756d846
Tim La <tlamarketing111@gmail.com>
2022-04-01 21:26:30 -0400

    change button text to see if site working properly

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -44,7 +44,7 @@ const Welcome = () => {
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd"
 
                     >
-                        <p className="text-white text-base font-semibold">Connect Wallet</p>
+                        <p className="text-white text-base font-semibold">Connect Your Wallet</p>
                     </button>
 
                     <div className="grid sm:grid-cols-3 grid-cols2 w-full mt-10">
