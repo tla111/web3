4126388c1088622e32d27da2e42eff8d5d41a559
Tim La <tlamarketing111@gmail.com>
2022-04-03 15:18:08 -0400

    changed text back

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -80,7 +80,7 @@ const Welcome = () => {
                             </div>
                             <div>
                                 <p className="text-white font-light text-sm">
-                                    Address ajaja
+                                    Address
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
