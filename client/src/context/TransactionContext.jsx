6eb0be81d1a41ac8616a3a9a583539f4576b59ea
Tim La <tlamarketing111@gmail.com>
2022-04-04 13:37:47 -0400

    created context folder and component

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import {ethers} from 'ethers';
+
+import {contractABI, contractAddress} from '../utils/constants';
+
+export const TransactionContext = React.createContext();
+
+const {ethereum} = window;
+
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionContract = new ethers.Contract()
+
+    console.log({
+        provider,
+        signer,
+        t
+    });
+}
