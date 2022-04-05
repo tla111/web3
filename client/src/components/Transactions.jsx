1168bc62c1ab1acb2221b8f38f9dfc5405dc8971
Tim La <tlamarketing111@gmail.com>
2022-04-05 12:55:59 -0400

    able to properly display real and dummy data transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -62,17 +62,17 @@ const Transactions = () => {
                     </h3>
                 )}
 
-                <div className="flex flex-wrap justify-center items-center mt-10">
+                {/* <div className="flex flex-wrap justify-center items-center mt-10">
                     {transactions.reverse().map((transaction, i) => (
                         <TransactionsCard key={i} {...transaction} />
                     ))}
-                </div>
+                </div> */}
 
-                {/* <div className="flex flex-wrap justify-center items-center mt-10">
+                <div className="flex flex-wrap justify-center items-center mt-10">
                     {[...dummyData, ...transactions].reverse().map((transaction, i) => (
                         <TransactionsCard key={i} {...transaction} />
                     ))}
-                </div> */}
+                </div>
 
             </div>
         </div>
